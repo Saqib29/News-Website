752a9bbf87fc47dc9b2223791c8690c26d99f93a
Saqib <saqib29111@gmail.com>
2021-01-22 00:56:33 +0600

    pull data through API from nws website

--- resources/routes/news.js
+++ resources/routes/news.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const axios   = require('axios');
+
 
 router.get('/', async (req, res) => {
-    res.render('news');
+    try{
+        const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts/`);
+        // console.log(newsAPI.data);
+        res.render('news', { articles : newsAPI.data });
+
+    }catch(err) {
+        if(err.response) {
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+        } else if(err.requiest) {
+            console.log(err.requiest);
+        } else {
+            console.error('Error', err.message);
+        }
+    }
 });
 
 module.exports = router;
