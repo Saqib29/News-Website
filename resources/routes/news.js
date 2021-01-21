f1cbb938b8b6ff9cc8f8fd6f3fc0ff427b96f60d
Saqib <saqib29111@gmail.com>
2021-01-22 01:47:39 +0600

    add css style & frontend

--- resources/routes/news.js
+++ resources/routes/news.js
@@ -7,16 +7,22 @@ router.get('/', async (req, res) => {
     try{
         const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts/`);
         // console.log(newsAPI.data);
-        res.render('news', { articles : newsAPI.data });
+        res.render('news', { articles : newsAPI.data }); 
 
     }catch(err) {
         if(err.response) {
+            res.render('news', { articles : null }); 
+
             console.log(err.response.data);
             console.log(err.response.status);
             console.log(err.response.headers);
         } else if(err.requiest) {
+            res.render('news', { articles : null }); 
+
             console.log(err.requiest);
         } else {
+            res.render('news', { articles : null }); 
+
             console.error('Error', err.message);
         }
     }
